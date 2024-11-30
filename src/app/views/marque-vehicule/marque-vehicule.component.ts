f8b8a3b273742607c9ef3d8e8a73048e69276e3c
devBakary <bakdia194@gmail.com>
2024-11-30 21:53:29 +0000

    marque de vehicule en cours

--- src/app/views/marque-vehicule/marque-vehicule.component.ts
+++ src/app/views/marque-vehicule/marque-vehicule.component.ts
@@ -1,13 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 
 @Component({
   selector: 'app-marque-vehicule',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './marque-vehicule.component.html',
   styleUrl: './marque-vehicule.component.scss'
 })
 export class MarqueVehiculeComponent {
+  showSearchBar: boolean = false;
 
+  toggleSearchBar(): void {
+    this.showSearchBar = !this.showSearchBar;
+  }
 }
