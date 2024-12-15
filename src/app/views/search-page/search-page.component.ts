c18a338220cdd9f18f51f780304b569a22a696d3
devBakary <bakdia194@gmail.com>
2024-12-15 22:37:54 +0000

    addiction du zoom

--- src/app/views/search-page/search-page.component.ts
+++ src/app/views/search-page/search-page.component.ts
@@ -9,4 +9,21 @@ import { RouterModule } from '@angular/router';
 })
 export class SearchPageComponent {
 
+  zoomLevels: number[] = [1, 1.5, 2, 2.5]; // Les différents niveaux de zoom
+  currentZoomIndex: number = 0; // Index du niveau de zoom actuel
+  zoomStyle: string = 'scale(1)';
+  zoomTransition: string = 'transform 0.3s ease'; // Transition douce
+
+  zoomIn() {
+    // Passer au niveau de zoom suivant
+    this.currentZoomIndex = (this.currentZoomIndex + 1) % this.zoomLevels.length;
+    const zoomLevel = this.zoomLevels[this.currentZoomIndex];
+    this.zoomStyle = `scale(${zoomLevel})`;
+  }
+  zoomOut() {
+    // Passer au niveau de zoom avant
+    this.currentZoomIndex = (this.currentZoomIndex - 1 + this.zoomLevels.length) % this.zoomLevels.length;
+    const zoomLevel = this.zoomLevels[this.currentZoomIndex];
+    this.zoomStyle = `scale(${zoomLevel})`;
+  }
 }
