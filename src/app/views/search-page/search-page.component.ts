7d848a7b9f2235d2748782e1c23386fcf47f3ab8
devBakary <bakdia194@gmail.com>
2024-12-19 20:18:44 +0000

    commit avant pull de sidi

--- src/app/views/search-page/search-page.component.ts
+++ src/app/views/search-page/search-page.component.ts
@@ -9,19 +9,17 @@ import { RouterModule } from '@angular/router';
 })
 export class SearchPageComponent {
 
-  zoomLevels: number[] = [1, 1.5, 2, 2.5]; // Les différents niveaux de zoom
-  currentZoomIndex: number = 0; // Index du niveau de zoom actuel
+  zoomLevels: number[] = [1, 1.5, 2, 2.5];
+  currentZoomIndex: number = 0;
   zoomStyle: string = 'scale(1)';
-  zoomTransition: string = 'transform 0.3s ease'; // Transition douce
+  zoomTransition: string = 'transform 0.3s ease';
 
   zoomIn() {
-    // Passer au niveau de zoom suivant
     this.currentZoomIndex = (this.currentZoomIndex + 1) % this.zoomLevels.length;
     const zoomLevel = this.zoomLevels[this.currentZoomIndex];
     this.zoomStyle = `scale(${zoomLevel})`;
   }
   zoomOut() {
-    // Passer au niveau de zoom avant
     this.currentZoomIndex = (this.currentZoomIndex - 1 + this.zoomLevels.length) % this.zoomLevels.length;
     const zoomLevel = this.zoomLevels[this.currentZoomIndex];
     this.zoomStyle = `scale(${zoomLevel})`;
